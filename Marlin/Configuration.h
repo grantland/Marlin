128d2d85e36cfbc0cabbca35c7e02f8b2da62ebb
Grantland Chew <grantlandchew@gmail.com>
2021-04-14 00:18:55 -0700

    Enable BLTouch
    Used [Adjustable BL-Touch sensor mount for Ender 3 and CR10 (with STEP file)](https://www.thingiverse.com/thing:3148733) remixed with [Cr-10 Ender 3 Hotend Fan Duct](https://www.thingiverse.com/thing:3606112)
    
    Followed instructions for 3+2 pin wiring utilizing Z-probe pins: https://www.youtube.com/watch?v=neS7lB7fCww

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -972,7 +972,7 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-//#define BLTOUCH
+#define BLTOUCH
 
 /**
  * Touch-MI Probe by hotends.fr
@@ -1064,7 +1064,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -14, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1396,7 +1396,7 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
+#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
@@ -1589,7 +1589,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing.
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-//#define Z_SAFE_HOMING
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
